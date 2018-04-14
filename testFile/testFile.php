ebe4c081e171fd911d1baf1273f7bd55b16fdce4
Godfadatun <36653089+Godfadatun@users.noreply.github.com>
2018-04-14 12:20:52 +0100

    Add files via upload

---
+++ testFile/testFile.php
@@ -0,0 +1,29 @@
+<htm>
+    
+<head>
+    <link rel="stylesheet" href="testFile.css">
+    <title>testfile</title>
+</head>    
+<body style="background-color: black">
+    <p class="divvy">#HNGInternship4</p>
+    <div class="div1">
+        <h1>
+            Welcome
+        </h1>
+        
+    </div>
+<div class="div2">
+    
+    <img src="qrcode.41955844.png" alt="MyQRcode" style="width:width;height:height;" class="div3">
+<?php
+    date_default_timezone_set("Africa/Lagos");
+    echo "  " . date("h:i:sa");
+?>
+
+    
+</div>    
+
+    
+</body>
+</htm>
+
